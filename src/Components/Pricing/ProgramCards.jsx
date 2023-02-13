9fa03a1dd4396a9f240bdce04ec732e9537b8bbc
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-02-13 10:45:35 +0100

    fixed

--- src/Components/Pricing/ProgramCards.jsx
+++ src/Components/Pricing/ProgramCards.jsx
@@ -145,7 +145,7 @@ export const OnsiteCard = () => {
           "This is for professionals who prefers physical training to learn the required skills to become a Product Manager"
         }
         tuitionFee={"₦200,000"}
-        duration={"8 Weeks"}
+        duration={"10 Weeks"}
         mobileHeight={"100%"}
         height={"73.4375rem"}
        
