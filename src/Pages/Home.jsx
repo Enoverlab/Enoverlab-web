aa335b247dce25ce474c441ae8f881a648dc2da3
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-03-06 10:53:07 +0100

    fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import Sessions from '../Components/HomeComponents/Sessions'
 import Started from '../Components/HomeComponents/Started'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
-import Banner from '../Components/HomeComponents/Banner'
+// import Banner from '../Components/HomeComponents/Banner'
 // import { theme } from '../Utils/Theme'
 const Home = () => {
   return (
@@ -19,7 +19,7 @@ const Home = () => {
     }}
     >
        <Header bgColor="#FFFFFF" bgColorMobile="#F5FCFF"/>
-       <Banner />
+       {/* <Banner /> */}
         <Hero/>
         <About/>
         <Training/>
