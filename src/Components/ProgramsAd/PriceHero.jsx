0935a2deafe051f78b9d5fe8806e5a952b659656
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-10-17 08:34:54 +0100

    ficed

--- src/Components/ProgramsAd/PriceHero.jsx
+++ src/Components/ProgramsAd/PriceHero.jsx
@@ -100,10 +100,11 @@ const PriceHero = () => {
           <div id="basic">
             <BasicCard/>
           </div>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/jXR2CYoci_4?si=RQNw6ZY0L8XLO8il" title="YouTube video player" frameborder="0" allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <div id="advanced">
             <AdvancedCard/>
           </div>
-          
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/CnlDAo2EHVU?si=iioex-ResHalig_y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           
           {/* <div id="advanced">
             <Timer/>
@@ -157,7 +158,7 @@ const PriceHero = () => {
           <div id="onsite">
           <OnsiteCard/>
           </div>
-
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/iPMxwnmj7sk?si=IIoJGezZSvZXKOUI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           {/* <div  id="advanced">
           <TechnicalPM/>
           </div> */}
