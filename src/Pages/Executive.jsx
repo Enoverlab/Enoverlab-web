2585bed3a36514cbf6e361e95fdc149dba046107
oladee <deemajor230600@gmail.com>
2024-05-20 14:57:29 +0100

    exec

--- src/Pages/Executive.jsx
+++ src/Pages/Executive.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import styled from 'styled-components'
-import ExecutiveAbout from '../Components/Executive/ExecutiveAbout'
-import Hero from '../Components/Executive/Hero'
-import Training from '../Components/Executive/Training'
-import Footer from '../Components/Footer'
+import KenyanHero from '../Components/Executive/ExecutiveHero'
 import Header from '../Components/Header'
-import Started from '../Components/HomeComponents/Started'
+import Footer from '../Components/Footer'
+import { ClassTools } from '../Components/Pricing/ClassTools'
+import Access from '../Components/Executive/Access'
+import ProgramDetails from '../Components/Executive/ProgramDetails'
+import SuccessStories from '../Components/HomeComponents/SuccessStories'
+import { FaqSection } from "../Components/Pricing/FaqSection"
+import { H2 } from '../Utils/styled/Typograpyhy'
+import Formi from '../Components/Kenya/Form'
+import {useState, useEffect} from "react"
+import LoadingScreen2 from '../Components/LoadingScreen2'
+
 const Executive = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },4000)
+  },[])
   return (
-    <StyledExecutive>
-        <Header bgColor="#FFFDF7"  bgColorMobile="#FFFDF7"/>
-        <Hero/>
-        <ExecutiveAbout/>
-        <Training/>
-        <Started/>
-        <Footer/>
-    </StyledExecutive>
+    <div>
+        {loading ? <LoadingScreen2/> :
+        <div>
+          <Header/>
+        <KenyanHero/>
+        <Access/>
+        <ProgramDetails/>
+        <ClassTools/>
+        <Formi/>
+        <div>
+        <H2 mt="4rem" tA="center" color='var(--TItle-Text)'>What Our Alumni Say</H2>
+        <SuccessStories/>
+        </div>
+        <FaqSection/>
+        <Footer p="5rem 9.6rem 0 9.6rem"/>
+        </div>}
+    </div>
   )
 }
 
 export default Executive
-
-const StyledExecutive = styled.div``
