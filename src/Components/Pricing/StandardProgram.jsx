5d0a9d0bb35bf97ca059a82641723b0922a9b493
oladee <deemajor230600@gmail.com>
2024-07-15 12:14:11 +0100

    finished added map api

--- src/Components/Pricing/StandardProgram.jsx
+++ src/Components/Pricing/StandardProgram.jsx
@@ -17,49 +17,55 @@ import {useState, useEffect} from "react";
 const StandardProgram = () => {
   const [location, setLocation] = useState('')
   const [isError,setIsError] = useState(false)
-  const [trainingLocation, setTrainingLocation] = useState({location : "Select Location to see address", tuition : '300,000', link : ''})
+  const [trainingLocation, setTrainingLocation] = useState({location : "Select Location to see address", tuition : '300,000', link : '', mapIframe : ''})
   useEffect(()=>{
     switch(location){
       case "Lekki":
         setTrainingLocation({
           location : 'Lekki Gateview Plaza, Admiralty way, Lekki Phase 1',
           tuition : '300,000',
-          link : 'https://forms.gle/RubzGLnYfKgShe4N7'
+          link : 'https://forms.gle/RubzGLnYfKgShe4N7',
+          mapIframe : <iframe title="Lekki" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.644401811104!2d3.453612374484845!3d6.439691993551541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf59eb8a97233%3A0x32b5defb8f41b624!2sGateview%20Plaza!5e0!3m2!1sen!2sng!4v1721039921603!5m2!1sen!2sng" width="100%" height="450" style={{border : 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         })
         break;
       case "Ibadan":
         setTrainingLocation({
           location : 'Cafe One, Palms Mall, Ring Road, Ibadan',
           tuition : '250,000',
-          link :  "https://forms.gle/CsYYXENLZNmjnsfK6"
+          link :  "https://forms.gle/CsYYXENLZNmjnsfK6",
+          mapIframe : <iframe title="Ibadan" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9143544673657!2d3.8634040744943348!3d7.3634969926456275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398decfa59eb47%3A0x97b5292d9159b2d8!2sCaf%C3%A9%20One%2C%20The%20Palms%20Mall%2C%20Ring%20Road%2C%20Ibadan!5e0!3m2!1sen!2sng!4v1721039815052!5m2!1sen!2sng" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         })
         break;
         case "Anambra":
           setTrainingLocation({
             location : 'Kodex Africa, Plot No. A/127, Iyiagu Housing Estate, Awka,Anambra',
             tuition : '250,000',
-            link :  "https://forms.gle/YWw8jFN42sDoPQiT8"
+            link :  "https://forms.gle/YWw8jFN42sDoPQiT8",
+            mapIframe : <iframe title="Anambra" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3051337147767!2d7.053362574482788!3d6.223437493764614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104379af706c6619%3A0x2598afc465c51641!2sKodex%20Africa!5e0!3m2!1sen!2sng!4v1721040675268!5m2!1sen!2sng" width="600" height="450" style={{border : 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           })
           break;
       case "Abuja":
         setTrainingLocation({
           location : '22 Kumasi Cres, Wuse 2, Abuja',
           tuition : '300,000',
-          link : "https://forms.gle/Gp7G4tN6QsxaPnds7"
+          link : "https://forms.gle/Gp7G4tN6QsxaPnds7",
+          mapIframe : <iframe title="Abuja" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.7928300264903!2d7.476952574515317!3d9.082628890980839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0af7b326c723%3A0xd8be18bcf35b950b!2s22%20Kumasi%20Cres%2C%20Wuse%2C%20Abuja%20904101%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1721039639625!5m2!1sen!2sng" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         })
         break;
-        case "Berger":
+        case "Ikeja":
           setTrainingLocation({
-            location : '75b Ogunnusi Rd, Isheri, Ojodu Berger, Lagos Berger',
+            location : 'The Philippi Centre, Oluwalogbon House, Plot A Obafemi Awolowo Way, Alausa, Ikeja',
             tuition : '300,000',
-            link : 'https://forms.gle/p1FhbPHk5muJGpdK6'
+            link : 'https://forms.gle/p1FhbPHk5muJGpdK6',
+            mapIframe : <iframe title="Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.270366461118!2d3.3545898!3d6.613292100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b934641e6428f%3A0x2131b57bcd1266a0!2sEridanSpace%20Ikeja%20Lagos!5e0!3m2!1sen!2sng!4v1721037913886!5m2!1sen!2sng" width="100%" height="450" style={{border : 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           })
           break;
       default:
         setTrainingLocation({
           location : 'Select Location to see address',
           tuition : '300,000',
-          link : ''
+          link : '',
+          mapIframe : ''
         })
         break;
     }
@@ -244,7 +250,7 @@ const StandardProgram = () => {
                           <option value="Lekki">LEKKI</option>
                           <option value="Ibadan">IBADAN</option>
                           <option value="Abuja">ABUJA</option>
-                          <option value="Berger">BERGER</option>
+                          <option value="Ikeja">IKEJA</option>
                           <option value="Anambra">ANAMBRA</option>
                         </select>
                         {isError && <p className="err">Please select Your Preferred Location</p>}
@@ -301,6 +307,9 @@ const StandardProgram = () => {
                   </div>
                 </form>
                 </div>
+                <div className="mapMobileIframe">
+                {trainingLocation.mapIframe}
+                </div>
               <div className="breakdown">
                 <H4
                   mdfs="1.58vw"
@@ -331,6 +340,9 @@ const StandardProgram = () => {
             </Flex>
           </SwiperSlide>
         </Swiper>
+      </div>
+      <div className="mapLaptopIframe">
+      {trainingLocation.mapIframe}
       </div>
     </StyledProgram>
   );
@@ -490,6 +502,13 @@ const StyledProgram = styled.div`
     font-size: 1.2rem;
     margin: 0.5rem 0;
   }
+  .mapMobileIframe{
+    display: block;
+    padding: 30px 20px 30px 5px ;
+  }
+  .mapLaptopIframe{
+    display: none;
+  }
   @media (min-width: 1024px) {
     padding: 110px 9.6rem;
     header {
@@ -575,6 +594,13 @@ const StyledProgram = styled.div`
     #locale{
     width: 19rem;
     font-size: 18px;
+  }
+  .mapMobileIframe{
+    display: none;
+  }
+  .mapLaptopIframe{
+    display: block;
+    padding: 30px 0px;
   }
 
   }
