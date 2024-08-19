3eab59ba83a5d8fe8fe25470bf19f8e08a7183e8
oladee <deemajor230600@gmail.com>
2024-08-19 21:55:58 +0100

    fini

--- src/constants/index.js
+++ src/constants/index.js
@@ -217,8 +217,7 @@ export const OnsiteProgram = [
 export const PrivateCoaching = [
   "One-on-one coaching and mentorship ONLINE for 6 weeks or less",
   "Flexible class time schedule",
-  `Guaranteed Product Manager Job recommendations 
-  PS: YOU MUST BE READY TO PLAY YOUR PART!**`,
+  `Guaranteed Product Manager Job recommendations`,
   "Close support and guidance till you land a Product Manager role",
   "International certification accredited by the American Council of Training and Development (ACTD)",
   "Lifetime access to Product management resources",
