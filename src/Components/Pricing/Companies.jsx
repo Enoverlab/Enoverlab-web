b23001efe9b1224e4bd874b76117cde33d98b562
oladee <deemajor230600@gmail.com>
2024-08-28 18:36:00 +0100

    included more company logos

--- src/Components/Pricing/Companies.jsx
+++ src/Components/Pricing/Companies.jsx
@@ -2,21 +2,38 @@ import styled from "styled-components";
 import Marquee from "react-fast-marquee";
 import fidelity from "../../assets/icon/fidelity.svg"
 import seamlesshr from "../../assets/icon/seamlesshr.png"
-// import borderlesshr from "../../assets/icon/borderless.png"
+import borderlesshr from "../../assets/icon/borderless.png"
 import damdam from "../../assets/icon/damdam.png"
 import sterling from "../../assets/icon/sterling.png"
 import etranzact from "../../assets/icon/etransact.png"
 import interswitch from  "../../assets/icon/interswitch.svg"
+import cambridge from "../../assets/icon/cambridge.png"
+import unidroit from "../../assets/icon/unidroit.png"
+import access from "../../assets/icon/access.png"
+import opay from "../../assets/icon/opay.jpeg"
+import renmoney from "../../assets/icon/renmoney.png"
+import ulospaces from "../../assets/icon/ulospaces.svg"
+import gtbank from "../../assets/icon/gtbank.jpeg"
 
 const Companies = () => {
   return (
     <StyledCompanies>
       <header>Where Our Alumni Work</header>
+      <Marquee>
+        <img src={borderlesshr } alt="borderlesshr_logo" />
+        <img src={cambridge} alt="cambridge_logo" />
+        <img src={unidroit} alt="unidroit_logo" />
+        <img src={access} alt="unidroit_logo" />
+        <img src={opay} alt="unidroit_logo" />
+        <img src={renmoney} alt="renmoney_logo"/>
+        <img src={ulospaces} alt="renmoney_logo"/>
+      </Marquee>
       <Marquee className="marquee">
         <img src={fidelity} alt="" />
         <img src={seamlesshr} alt="" />
         <img src={interswitch} alt="" />
         <img src={damdam} alt="" />
+        <img src={gtbank} alt="" />
         <img src={sterling} alt="" />
         <img src={etranzact} alt="" />
       </Marquee>
@@ -28,14 +45,16 @@ export default Companies
 
 
 const StyledCompanies = styled.div`
-    margin: 3rem 0;
+    margin: 4rem 0;
     box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.25);
-    header {
-    padding: 4.1rem 2.4rem;
-    font-weight: 700;
-    color: var(--Body-Text);
-    font-size: 2rem;
     padding-bottom: 2rem;
+    header {
+      padding: 3.1rem 2.4rem;
+      font-weight: 700;
+      text-align: center;
+      color: var(--Body-Text);
+      font-size: 2rem;
+      padding-bottom: 1rem;
     }
     img{
     width: 10rem;
@@ -43,7 +62,7 @@ const StyledCompanies = styled.div`
     margin: 1rem 3rem;
     }
     @media (min-width: 1024px) {
-    margin: 6rem 0 0 0;
+    margin: 8rem 0 0 0;
     header {
       padding: 3.2rem 9.6rem;
       font-size: 3rem;
