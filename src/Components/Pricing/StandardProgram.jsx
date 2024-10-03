ed359ea9f91f4cbe9d66caac51445904517ae667
oladee <deemajor230600@gmail.com>
2024-10-03 11:47:14 +0100

    done

--- src/Components/Pricing/StandardProgram.jsx
+++ src/Components/Pricing/StandardProgram.jsx
@@ -66,7 +66,7 @@ const StandardProgram = () => {
           weekIcon : weeks20,
           link :  "https://forms.gle/CsYYXENLZNmjnsfK6",
           mapIframe : <iframe title="Ibadan" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.9143544673657!2d3.8634040744943348!3d7.3634969926456275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398decfa59eb47%3A0x97b5292d9159b2d8!2sCaf%C3%A9%20One%2C%20The%20Palms%20Mall%2C%20Ring%20Road%2C%20Ibadan!5e0!3m2!1sen!2sng!4v1721039815052!5m2!1sen!2sng" width="100%" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
-          startDate : "Sept 21, 2024"
+          startDate : "Jan 18, 2025"
         })
         break;
         case "Anambra":
