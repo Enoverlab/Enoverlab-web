970f0e7e14c7ae42212b19916e7253e2ff2f7ef1
oladee <deemajor230600@gmail.com>
2024-12-21 12:49:27 +0100

    cleared unused files created payment folder and page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Home from "./Pages/Home";
 import Pricing from "./Pages/Pricing";
 import About from "./Pages/About";
 import Blog from "./Pages/Blog";
-import Pay from "./Pages/Pay";
 import Resources from "./Pages/Resources";
 import Corporate from "./Pages/Corporate";
 import { Routes, Route} from "react-router-dom";
@@ -14,7 +13,6 @@ import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { theme } from "./Utils/Theme";
 import BlogContentPage from "./Pages/BlogContentPage";
 import AOS from "aos";
-import CheckoutPage from "./Pages/CheckoutPage"
 import Executive from "./Pages/Executive";
 import Sales from "./Pages/Sales";
 import Exclusive from "./Pages/Exclusive";
@@ -71,9 +69,7 @@ function App() {
         <Route path="/aipmp" element={<AI/>} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
-        <Route path="/pay" element={<Pay />} />
         <Route path="/blog/:id" element={<BlogContentPage />} />
-        <Route path="/pay/:id" element={<CheckoutPage />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/corporate" element={<Corporate />} />
         <Route path="/executive" element={<Executive/>} />
