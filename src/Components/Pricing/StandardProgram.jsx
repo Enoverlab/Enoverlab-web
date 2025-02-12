77332e0e5dc6fd73fa773db72519d31b8fd4929a
oladee <deemajor230600@gmail.com>
2025-02-12 13:45:21 +0100

    doney

--- src/Components/Pricing/StandardProgram.jsx
+++ src/Components/Pricing/StandardProgram.jsx
@@ -354,7 +354,7 @@ const StandardProgram = () => {
                           fw="600"
                           color="var(--Body-Text)"
                         >
-                          Monday - Friday
+                          Flexible
                         </H2>
                        </div>
                        <div>
@@ -367,7 +367,7 @@ const StandardProgram = () => {
                           fw="600"
                           color="var(--Body-Text)"
                         >
-                          2 hours daily
+                          Flexible
                         </H2>
                        </div>
                         <div>
