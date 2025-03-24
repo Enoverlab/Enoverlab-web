4196087e2b41714da3be97be9d9a6bef11ba410d
oladee <deemajor230600@gmail.com>
2025-03-24 14:10:17 +0100

    latest development

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -15,7 +15,7 @@ import {useState, useEffect} from "react"
 import LoadingScreen2 from '../Components/LoadingScreen2'
 import Guarantee from '../Components/Pricing/Guarantee'
 import Companies from '../Components/Pricing/Companies'
-import AccessModal from '../Components/Assessment/Landing/AccessModal'
+// import AccessModal from '../Components/Assessment/Landing/AccessModal'
 const Pricing = () => {
   const [loading, setLoading] = useState(true)
   useEffect(()=>{
@@ -36,7 +36,7 @@ const Pricing = () => {
       <Testimonials/>
       <Testimonies/>
       <Explore/>
-      <AccessModal/>
+      {/* <AccessModal/> */}
       <FaqSection/>
     </StyledPricing>
     <Footer p="5rem 9.6rem"/>
