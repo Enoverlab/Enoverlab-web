4ba8eb8b1be4db914d654c5d3e6bb858ec9e9886
oladee <deemajor230600@gmail.com>
2025-04-25 12:12:53 +0100

    latest

--- src/Components/Pricing/StandardProgram.jsx
+++ src/Components/Pricing/StandardProgram.jsx
@@ -22,7 +22,7 @@ const StandardProgram = () => {
   const [location, setLocation] = useState('')
   const [isError,setIsError] = useState(false)
   const centerDetails = useContext(CenterContext)
-  const admissionClosed = ['']
+  const admissionClosed = ['Ikeja-Weekend']
 
   const [trainingLocation, setTrainingLocation] = useState({place : '',location : "Select Location to see address", tuition : '300,000', link : '', mapIframe : '', weeks : "", startDate : "", weekIcon : weeks20,trainingDay : "", trainingTime : '', classType : '', discount : ''})
   
