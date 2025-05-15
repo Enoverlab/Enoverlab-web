726a80d11a20d09a35f353c66661b85a260cf6a6
Oladimeji Momoh <deemajor230600@gmail.com>
2025-05-15 08:20:44 +0100

    Update CenterContext.jsx

--- src/context/CenterContext.jsx
+++ src/context/CenterContext.jsx
@@ -370,7 +370,7 @@ const centerDetails = {
     'Ikeja-Weekday' : {
         place : 'Ikeja-Weekday',
         location : 'Ikeja Town Square, Obafemi Awolowo Way, Ikeja, 101233, Lagos',
-        trainingDay : "Every Tuesday & Friday",
+        trainingDay : "Every Tuesday & Thursday",
         classType : 'weekday',
         trainingTime : "1pm - 4pm",
         tuition : '400,000',
@@ -392,7 +392,7 @@ const centerDetails = {
             } 
         },
         mapIframe : <iframe title="ikeja cafe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.2834057797636!2d3.3533708!3d6.611666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b9349480e610d%3A0x151dbb19335e7eb2!2sCaf%C3%A9%20One%20Ikeja!5e0!3m2!1sen!2sng!4v1744808252160!5m2!1sen!2sng" width="100%" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
-        startDate : "May 27, 2025",
+        startDate : "June 10, 2025",
     },
     'Ikeja-Weekend' : {
         place : 'Ikeja-Weekend',
