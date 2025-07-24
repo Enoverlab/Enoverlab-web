1fb86ee9e4a727a83cecdfdb7d25a44011944ba7
Oladimeji Momoh <deemajor230600@gmail.com>
2025-07-24 11:00:53 +0100

    Update CenterContext.jsx

--- src/context/CenterContext.jsx
+++ src/context/CenterContext.jsx
@@ -256,7 +256,7 @@ const centerDetails = {
             } 
         },
         mapIframe : <iframe title="Lekki" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.644401811104!2d3.453612374484845!3d6.439691993551541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf59eb8a97233%3A0x32b5defb8f41b624!2sGateview%20Plaza!5e0!3m2!1sen!2sng!4v1721039921603!5m2!1sen!2sng" width="100%" height="450" style={{border : 0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
-        startDate : "August 12, 2025",
+        startDate : "September 9, 2025",
     },
     "Lekki-Weekend" : {
         place : 'Lekki',
@@ -392,7 +392,7 @@ const centerDetails = {
             } 
         },
         mapIframe : <iframe title="Ikeja Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.270366461118!2d3.3545898!3d6.613292100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b934641e6428f%3A0x2131b57bcd1266a0!2sEridanSpace%20Ikeja%20Lagos!5e0!3m2!1sen!2sng!4v1721037913886!5m2!1sen!2sng" width="100%" height="450" style={{border : 0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>,
-        startDate : "August 12, 2025",
+        startDate : "September 9, 2025",
     },
     'Ikeja-Weekend' : {
         place : 'Ikeja-Weekend',
