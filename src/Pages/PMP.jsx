45e4fc46fb90b8f7c764b7750c472be7602bcc77
oladee <deemajor230600@gmail.com>
2026-04-11 20:30:44 +0100

    latest update

--- src/Pages/PMP.jsx
+++ src/Pages/PMP.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import styled from 'styled-components'
-import PriceHero from '../Components/PMP/PriceHero'
+import PMPHero from '../Components/PMP/PMPHero'
 import Header from '../Components/Header'
-import PricingAbout from '../Components/Pricing/PricingAbout'
-import Testimonies from '../Components/Pricing/Testimonies'
-import SuccessStories from '../Components/Pricing/SuccessStories'
 import Footer from '../Components/Footer'
-// import PricingBanner from '../Components/Pricing/PricingBanner'
-
+import { ClassTools } from '../Components/Pricing/ClassTools'
+import Access from '../Components/PMP/Access'
+import ProgramDetails from '../Components/PMP/ProgramDetails'
+import SuccessStories from '../Components/HomeComponents/SuccessStories'
+import { FaqSection } from "../Components/Pricing/FaqSection"
+import { H2 } from '../Utils/styled/Typograpyhy'
+import Formi from '../Components/Kenya/Form'
+import {useState, useEffect} from "react"
+import LoadingScreen2 from '../Components/LoadingScreen2'
+import Guarantee from '../Components/Pricing/Guarantee'
+import Companies from '../Components/Pricing/Companies'
 
 const PMP = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },4000)
+  },[])
+  useEffect(()=>{
+    document.body.classList.remove('bodyStiff')
+  },[loading])
   return (
-    <StyledPricing>
-          <Header bgColor="#FFFDF7"  bgColorMobile="#FFFDF7"/>
-          {/* <PricingBanner/> */}
-        <PriceHero/>
-        <PricingAbout/>
-        <Testimonies/>
+    <div>
+        {loading ? <LoadingScreen2/> :
+        <div>
+          <Header/>
+        <PMPHero/>
+        <Access/>
+        <ProgramDetails/>
+        <Guarantee/>
+        <Companies/>
+        <ClassTools/>
+        <div>
+        <H2 mt="4rem" tA="center" color='var(--TItle-Text)'>What Our Alumni Say</H2>
         <SuccessStories/>
-        <Footer/>
-    </StyledPricing>
+        </div>
+        <Formi/>
+        <FaqSection/>
+        <Footer p="5rem 9.6rem 0 9.6rem"/>
+        </div>}
+    </div>
   )
 }
 
 export default PMP
-
-const StyledPricing = styled.div``
